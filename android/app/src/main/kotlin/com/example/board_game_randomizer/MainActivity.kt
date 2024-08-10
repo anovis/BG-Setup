5c30d4a5f24e0f7c746ad76a895a10668bc3f743
austen <anovis@ymail.com>
2024-08-10 14:31:50 -0400

    1.3.0

--- android/app/src/main/kotlin/com/example/board_game_randomizer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.board_game_randomizer.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/game_data.dart
+++
@@ -1,4 +0,0 @@
-export 'package:board_game_randomizer/data/cascadia.dart';
-export 'package:board_game_randomizer/data/cascadia_landmarks.dart';
-export 'package:board_game_randomizer/data/res_arcana.dart';
-export 'package:board_game_randomizer/data/wingspan.dart';
